b05c859e3dbe0fe44c5247342cc72cadce10c0ed
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-05-14 09:10:57 +0530

    Implement user registration authentication API

---
+++ backend/controllers/authController.js
@@ -0,0 +1,57 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+// REGISTER USER
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // generate token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const connectDB = require("./config/db");
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+// ROUTES
+app.use("/api/auth", require("./routes/authRoutes"));
+
 
 app.get("/", (req, res) => {
   res.send("SmartCV API Running...");
